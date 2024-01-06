812b8baeb6e7b20ad6025a6056032b0d984ed4dc
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2024-01-06 13:25:53 +0530

    Update komac.rb

--- komac.rb
+++ komac.rb
@@ -2,29 +2,63 @@ class Komac < Formula
   desc "KOtlin MAnifest Creator for Windows Package Manager (WinGet)"
   homepage "https://github.com/russellbanks/Komac"
   license "GPL-3.0"
-  version "1.3.2"
+  version "2.0.0"
 
-  if OS.mac?
-    url "https://github.com/russellbanks/Komac/releases/download/v#{version}/Komac-#{version}.dmg"
-    sha256 "5ef282d3708a3f9a01e94baf0e962c5e94625dd68dd4956f389ac8c39ad9fdc8"
-  elsif OS.linux?
-    url "https://github.com/russellbanks/Komac/releases/download/v#{version}/komac_#{version}-1_amd64.deb"
-    sha256 "4e26307f8b28439fc1e63e5e466ef446144cc158a5e1c41f40f62c1ac80bde15"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/russellbanks/Komac/releases/download/v2.0.0/KomacPortable-macos-2.0.0-x64.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "KomacPortable-macos-2.0.0-x64" => "komac"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/russellbanks/Komac/releases/download/v2.0.0/KomacPortable-macos-2.0.0-arm64.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "KomacPortable-macos-2.0.0-arm64" => "komac"
+      end
+    end
   end
 
-  def install
-    if OS.mac?
-      system "/usr/bin/hdiutil", "mount", "-nobrowse", "-readonly", "-quiet", "#{cached_download}"
-      cp "-R", "/Volumes/Komac/Komac.app", "#{prefix}/"
-      system "/usr/bin/hdiutil", "unmount", "-quiet", "/Volumes/Komac"
-      bin.write_exec_script "#{prefix}/Komac.app/Contents/MacOS/Komac"
-    elsif OS.linux?
-      system "dpkg", "-x", "#{cached_download}", "#{prefix}"
-      bin.write_exec_script "#{prefix}/usr/bin/komac"
+  on_linux do
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/russellbanks/Komac/releases/download/v2.0.0/KomacPortable-linux-2.0.0-x64.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "KomacPortable-linux-2.0.0-x64" => "komac"
+      end
+    end
+    if Hardware::CPU.intel? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/russellbanks/Komac/releases/download/v2.0.0/KomacPortable-linux-2.0.0-x86.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "KomacPortable-linux-2.0.0-x86" => "komac"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/russellbanks/Komac/releases/download/v2.0.0/KomacPortable-linux-2.0.0-arm64.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "KomacPortable-linux-2.0.0-arm64" => "komac"
+      end
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/russellbanks/Komac/releases/download/v2.0.0/KomacPortable-linux-2.0.0-armv7.tar.gz"
+      sha256 ""
+
+      def install
+        bin.install "KomacPortable-linux-2.0.0-armv7" => "komac"
+      end
     end
   end
 
   test do
-    system "#{bin}/komac", "-h"
+    system "#{bin}/komac --help"
   end
 end
